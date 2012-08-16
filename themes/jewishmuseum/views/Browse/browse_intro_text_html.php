79ec9ef537f8ca2ec747794cde4b062b52b94948
JMP Developer <jmp.testing@gmail.com>
2012-08-16 15:20:34 +0200

    browse intro

--- themes/jewishmuseum/views/Browse/browse_intro_text_html.php
+++ themes/jewishmuseum/views/Browse/browse_intro_text_html.php
@@ -1,1 +1,25 @@
-Intro text.
+<div class="browse-intro">
+	<h1>Jak hledat v&nbsp;katalogu sbírek</h1>
+
+	<h2><b>Vyhledávání v&nbsp;plném textu</b></h2>
+	<ul>
+	<li>V poli <quot>Vyhledat ve sbírkách</quot> je možné vyhledávat na jakoukoli kombinaci slov či jejich částí ve všech relevantních polích digitálního katalogu. Na konci slov je možné použít * jako zástupný znak (například hledání na "terezín*" vyhledá "Terezín", "terezínské", atd.).</li>
+	<li>Následně je možné výsledek hledání zpřesňovat na základě klíčových slov, datace, lidí, organizací, míst a&nbsp;dalších parametrů.</li>
+	</ul>
+
+	<br />
+	<h2><b>Facetové vyhledávání</b></h2>
+	<div>
+	Sbírky je možné prohledávat také na základě strukturovaných vztahů k&nbsp;různým typům metadat (typy objektů, datace, lidé, organizace, místa, klíčová slova, sbírky). Toto "facetové" vyhledávání umožňuje postupně zužovat výsledek hledání.
+	Jednotlivé facety se zobrazí po kliknutí na "Prohlížet sbírky" nebo po zadání hledání v&nbsp;plném textu (pokud odpovídají výsledku hledání).
+	</div>
+
+	<br />
+	<h2><b>Prohlížení hierarchie sbírek</b></h2>
+	<div>K záznamům je možné také přistupovat prostřednictvím <?php print caNavLink($this->request, _t("hierarchie sbírek ŽMP"), "", "Hierarchy", "Show", "Index"); ?>.</div>
+
+	<br />
+	<h2><b>Přístup k&nbsp;datům</b></h2>
+	<div>I když Židovské muzeum v&nbsp;Praze usiluje o&nbsp;postupné uvolňování co nejvíce informací online, je část záznamů kvůli omezení práv ze strany původce či kvůli ochraně osobních dat přístupná pouze ve studovnách ŽMP. </div></div>
+
+</div>
