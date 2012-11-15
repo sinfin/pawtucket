ba6fe4d2c3b3fd03fecd76f9c3211bb005ce0c50
JMP Developer <jmp.testing@gmail.com>
2012-11-15 14:20:39 +0100

    filter empty

--- app/plugins/simpleGallery/views/jewishmuseum/set_info_html.php
+++ app/plugins/simpleGallery/views/jewishmuseum/set_info_html.php
@@ -131,7 +131,7 @@ function sizeClass($size, $sizes) {
 						$item .= caNavLink($this->request, $year['text'], '', '', 'Browse', 'clearAndAddCriteria', array('facet' => 'periodization_facet', 'id' => $year['search']));
 					}
 				$item .= '</h2>';
-				if ($va_item['set_item_label'] !== '[BLANK]') $item .= '<p>'.$va_item['set_item_label'].'</p>';
+				if ($va_item['set_item_label'] !== '[BLANK]' && $va_item['set_item_label'] !== '[PRÁZDNÉ]') $item .= '<p>'.$va_item['set_item_label'].'</p>';
 				$item .= '<div class="more">'.caNavLink($this->request, _t('detail'), '', 'Detail', 'Object', 'Show', array('object_id' =>  $object_id)).'</div>';
 			$item .= '</div>';
 		$item .= '</li>';
